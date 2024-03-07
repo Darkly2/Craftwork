372f47e0f80ada91f097fd83b63b106921251c6b
HomePC <chief.art-frolow@yandex.ru>
2024-03-07 23:38:39 +0300

    Create CraftItemFragment

--- app/src/main/java/com/example/craftwork/domain/craftItem/CraftItem.kt
+++ app/src/main/java/com/example/craftwork/domain/craftItem/CraftItem.kt
@@ -3,7 +3,7 @@ package com.example.craftwork.domain.craftItem
 data class CraftItem(
     val name: String,
     val craftType: String,
-    //val description: String,
+    val description: String,
     // val mainImage: Image,
     //val images: List<Image>,
     var id: String = UNDEFINED_ID, // when plugin Room and Firebase change to val
