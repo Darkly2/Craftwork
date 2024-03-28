99e89afd88953b2405cec566f65f884c237ad48f
HomePC <chief.art-frolow@yandex.ru>
2024-03-28 23:03:58 +0300

    Create parseIntent fun in CraftActivity. TODO fragment and editItem and addNewItem fun

--- app/src/main/java/com/example/craftwork/presentation/CraftItemFragment.kt
+++ app/src/main/java/com/example/craftwork/presentation/CraftItemFragment.kt
@@ -6,17 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.craftwork.R
+import com.example.craftwork.databinding.CraftItemFragmentBinding
 
 class CraftItemFragment: Fragment() {
+
+    private var _binding: CraftItemFragmentBinding? = null
+    private val binding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
-            R.layout.craft_item_fragment,
-            container,
-            false
-        )
+        _binding = CraftItemFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
